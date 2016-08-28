0264b13caef77b266848369317e7c919e8077567
Anton Sivertsson <sivertsson.anton@gmail.com>
2016-08-28 23:22:51 +0200

    Add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    entry: './app/index.jsx',
+    output: {
+        path: __dirname + '/bundle',
+        filename: 'bundle.js',
+        publicPath: 'http://localhost:8090/assets'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+              test: /\.css$/,
+              loaders: ['style', 'css']
+            }
+        ]
+    },
+    externals: {
+        'react': 'React'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx', '.css']
+    }
+}
