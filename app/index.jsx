11541c7b315985fba90ff808b354242631252b28
Anton Sivertsson <sivertsson.anton@gmail.com>
2016-08-28 23:22:34 +0200

    Add initial components

---
+++ app/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app.jsx';
+
+require('./styles/style.css');
+
+ReactDOM.render(<App />, document.getElementById('content'));
